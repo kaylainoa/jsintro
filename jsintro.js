5955d92adc01dfb1d402b7eccacd8f3a8c9d7648
kaylainoa <kinoa718@gmail.com>
2023-02-23 00:50:28 -0500

    jsintro

---
+++ jsintro.js
@@ -0,0 +1,8 @@
+var firstName = "Kayla";
+var lastName = "Inoa";
+var age = 16;
+
+window.alert("My name is " + firstName + " " + lastName);
+window.alert("I am " + age + " years old");
+
+console.log("I am having so much fun in class");
